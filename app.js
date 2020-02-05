cfed30b052e1e981970110da3b9deea5e3ccb78e
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-02-05 00:58:17 -0500

    overhaul of app.js, main function

--- app.js
+++ app.js
@@ -5,77 +5,84 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
+// main function
+  // declares empty team array variable
+  // calls function to determine whether or not to add another team member
+    // if yes, obtains role, name, id, email
+    // if no, calls function to build html page with the team array
+  // depending on role from previous function, obtains unique attribute and
+    // adds it to the employee object.
+  // push employee to team
+  // Repeats the cycle
+
 function buildTeam(){
-  assignManager()
-  .then(managerResponse => addRole(managerResponse))
-  .catch(err => console.log(err));
-}
-
-function assignManager(){
-    return new Promise((res) => {
-      inquirer
-      .prompt([
-          {
-            type: 'input',  
-            message: 'Enter Manager Name',
-            name: 'managerName'
-          },
-          {
-            type: 'input',
-            message: 'Manager Employee ID:',
-            name: 'managerID'
-          },
-          {
-            type: 'input',
-            message: 'Manager\'s email address:',
-            name: 'managerEmail'
-          },
-          {
-            type: 'input',
-            message: 'Manager\'s Office Phone #:',
-            name: 'managerPhone'
-          }
-      ])
-      .then(function(response){
-        const team = [];
-        const manager = new Manager(response.managerName,response.managerID,
-                                    response.managerEmail,response.managerPhone);                            
-        team.push(manager);
-        // console.log(team);
-        res(team);
-      });
-    });
-}
-
-function addRole(team){
-  return new Promise((res,rej) => {
-    if (team.find(el => el.getRole() === 'Manager')){
-      inquirer
-      .prompt({
-        type: 'list',
-        message: 'Assign your next teammate',
-        choices: ['Engineer','Intern','No more team members'],
-        name: 'newRole'
-      }).then(function(response){
-        console.log(response);
-        memberSwitchboard(response.newRole);
-        res(response);
-      })
+  const team = [];
+  addMore()
+  .then(addMoreResponse => emplDetails(addMoreResponse))
+  .then(emplDetailsResponse => testFunction(emplDetailsResponse))
+  .catch(err => {
+    if (err === 'Team Complete'){
+      generateHTML();
     } else {
-      rej("Error: Team needs a manager before you can assign more roles");
+      console.log(err);
     }
   });
 }
 
-function memberSwitchboard(str){
-  switch (str){
-    case 'Engineer':
-      console.log('Engineer');
-      break;
-    default:
-      console.log('something else');
-      break;
-  }
+function addMore(){
+  return new Promise((res,rej) => {
+    inquirer
+    .prompt({
+      type: 'confirm',
+      message: 'Add another team member?',
+      name: 'bool'
+    }).then(function(response){
+      response.bool ? res(response.bool) : rej('Team Complete');
+    });
+  });
 }
+
+function emplDetails(bool){
+  return new Promise((res,rej) => {
+    if (bool === true) {
+      inquirer
+      .prompt([
+        {
+          type: 'input',
+          message: 'Employee Name:',
+          name: 'employeeName'
+        },
+        {
+          type: 'list',
+          message: 'Employee Role:',
+          choices: ['Manager','Engineer','Intern'],
+          name: 'employeeRole'
+        },
+        {
+          type: 'input',
+          message: 'Employee ID:',
+          name: 'employeeID'
+        },
+        {
+          type: 'input',
+          message: "Employee's email address:",
+          name: 'employeeEmail'
+        }
+      ]).then(function(response){
+        res(response);
+      });
+    } else { rej('Oops, something went wrong.') }
+  });
+}
+
+function testFunction(obj){
+  console.log(obj);
+}
+
+
+// for now, don't code below this line. I want the generateHTML function to 
+// be the last thing before buildTeam()
+
+
 
 buildTeam();
